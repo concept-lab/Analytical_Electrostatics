7b8d46eaf7c396af6afe873b3f21e2a84a5cb9ca
Vincenzo Di Florio <vincenzo.diflorio97@gmail.com>
2025-02-13 17:13:47 +0100

    add Copyright notice

--- script/calc_energy_components0.m
+++ script/calc_energy_components0.m
@@ -1,3 +1,20 @@
+
+% Copyright (C) 2024-2025 Sergii V. Siryk
+% Copyright (C) 2024-2025 Vincenzo Di Florio
+% 
+% This program is free software: you can redistribute it and/or modify  
+% it under the terms of the GNU General Public License as published by  
+% the Free Software Foundation, either version 3 of the License, or  
+% (at your option) any later version.  
+% 
+% This program is distributed in the hope that it will be useful,  
+% but WITHOUT ANY WARRANTY; without even the implied warranty of  
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+% GNU General Public License for more details.  
+% 
+% You should have received a copy of the GNU General Public License  
+% along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 function [energy_reaction,energy_ionic,energy_Coulombic] = calc_energy_components0(particles_params,medium_params,n_max,particles_coefficients,energy_total, t_eps0_inv)
 % the parameter "energy_total" (the total electrostatic energy) is calculated from "multi_spheres_cg...m"
 kappa=medium_params.kappa;
--- script/multi_spheres_cg5.m
+++ script/multi_spheres_cg5.m
@@ -1,3 +1,20 @@
+
+% Copyright (C) 2024-2025 Sergii V. Siryk
+% Copyright (C) 2024-2025 Vincenzo Di Florio
+% 
+% This program is free software: you can redistribute it and/or modify  
+% it under the terms of the GNU General Public License as published by  
+% the Free Software Foundation, either version 3 of the License, or  
+% (at your option) any later version.  
+% 
+% This program is distributed in the hope that it will be useful,  
+% but WITHOUT ANY WARRANTY; without even the implied warranty of  
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+% GNU General Public License for more details.  
+% 
+% You should have received a copy of the GNU General Public License  
+% along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 function [particles_coefficients,energy,linear_system]=multi_spheres_cg5(particles_params,medium_params,n_max,array_clebschgordan,cg_n_max,tol_linsolver, t_eps0_inv)
 if numel(particles_params)<1
     error('At least one particle must be present. BYE.');
